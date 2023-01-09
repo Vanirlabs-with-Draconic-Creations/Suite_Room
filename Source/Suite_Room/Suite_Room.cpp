0b75248d0803272f6c86218dafd05d346ea4cf58
Vanir Labs <82656075+Vanir-Labs@users.noreply.github.com>
2023-01-09 16:18:48 -0500

    Base Commitment

---
+++ Source/Suite_Room/Suite_Room.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Suite_Room.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Suite_Room, "Suite_Room" );
